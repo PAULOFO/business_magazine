28a8e723a4e5fbaae54897b50028ab4f8d29938d
PAULOFO <paulofo.forever@gmail.com>
2020-03-28 02:43:47 -0300

    Botões de Ligar e Chat

--- lib/screens/product/product_screen.dart
+++ lib/screens/product/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:businessmagazine/models/ad.dart';
+import 'package:businessmagazine/screens/product/widgets/bottom_bar.dart';
 import 'package:businessmagazine/screens/product/widgets/description_panel.dart';
 import 'package:businessmagazine/screens/product/widgets/location_panel.dart';
 import 'package:businessmagazine/screens/product/widgets/main_panel.dart';
@@ -19,37 +20,42 @@ class ProductScreen extends StatelessWidget {
         title: const Text('Anúncio'),
         elevation: 0,
       ),
-      body: ListView(
+      body: Stack(
         children: <Widget>[
-          Container(
-            height: 280,
-            child: Carousel(
-              images: ad.images.map((f){
-                return FileImage(f);
-              }).toList(),
-              dotSize: 4,///Tamanho das bolinhas
-              dotSpacing: 15,///Espaço entre as bolinhas
-              dotBgColor: Colors.transparent,///Cor por trás das bolinhas
-              dotColor: Colors.pink,///Cor da bolinhas
-              autoplay: false,///true->Faz o Carousel se mover automaticamente
-            ),
+          ListView(
+            children: <Widget>[
+              Container(
+                height: 280,
+                child: Carousel(
+                  images: ad.images.map((f){
+                    return FileImage(f);
+                  }).toList(),
+                  dotSize: 4,///Tamanho das bolinhas
+                  dotSpacing: 15,///Espaço entre as bolinhas
+                  dotBgColor: Colors.transparent,///Cor por trás das bolinhas
+                  dotColor: Colors.pink,///Cor da bolinhas
+                  autoplay: false,///true->Faz o Carousel se mover automaticamente
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    MainPanel(ad),
+                    Divider(),
+                    DescriptionPanel(ad),
+                    Divider(),
+                    LocationPanel(ad),
+                    Divider(),
+                    UserPanel(ad),
+                    const SizedBox(height: 32,),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                MainPanel(ad),
-                Divider(),
-                DescriptionPanel(ad),
-                Divider(),
-                LocationPanel(ad),
-                Divider(),
-                UserPanel(ad),
-                const SizedBox(height: 32,),
-              ],
-            ),
-          ),
+         BottomBar(ad),
         ],
       ),
     );
